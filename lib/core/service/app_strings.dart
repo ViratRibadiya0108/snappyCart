0440e98422099a604348e08bfb8b53684ff5940f
Virat Patel <118610161+ViRatRibadiya1204@users.noreply.github.com>
2024-09-16 23:00:49 +0530

    feat: onbording_slide_screen_done

--- lib/core/service/app_strings.dart
+++ lib/core/service/app_strings.dart
@@ -1,14 +1,19 @@
 class AppStrings {
 
-  
+  //Home_Screen
   static const welcome = 'Welcome to SnappyCART';
   static const stitle = 'The home for a fashionista';
+  //onbording_screen
   static const discover = 'Discover something new';
   static const sdiscover = 'Special new arrivals just for you';
   static const update = 'Update trendy outfit';
   static const supdate = 'Favorite brands and hottest trends';
   static const explore = 'Explore your true style';
   static const sexplore = 'Relax and let us bring the style to you';
+  //auth_screen
+  static const creataccount = 'Create your account';
+  static const loginaccount ='Log into your account';
+  
 
   
 }
